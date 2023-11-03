cc71881ecf6eafda936686665b1687994e37f567
Adam Mathieson <adam@mathieson.dev>
2023-11-03 12:26:35 +0000

    + Button Highlights

--- js/components/MenuBar.js
+++ js/components/MenuBar.js
@@ -23,9 +23,13 @@ menu-bar > .container > .button {
     display: grid;
     max-width: 25vw;
     grid-template-rows: 50% 20%;
-    margin: 2pt;
     justify-items: center;
     align-content: center;
+    opacity: 1;
+    transition: background-color 50ms ease-out;
+    background: red;
+    padding: 2pt 1em;
+    cursor: pointer;
 }
 menu-bar > .container > .button-large > img {
     max-height: min(48pt, 10vw);
@@ -42,24 +46,35 @@ menu-bar > .container > .button > span {
     font-size: 12pt;
     height: 12pt;
 }
+menu-bar > .container > .button > i {
+        align-self: center;
+        margin-bottom: 5pt;
+}
+
+menu-bar > .container > .button:hover:not(.button-large) {
+    background: rgba(204,0,0);
+}
+menu-bar > .container > .button:active:not(.button-large) {
+    background: rgba(230,0,0);
+}
 </style>
 <div class="container">
-    <div class="button">
+    <div class="button" data-ref="toursButton">
         <i class="gg-user"></i>
         <span>Tours</span>
     </div>
-    <div class="button">
+    <div class="button" data-ref="trailsButton">
         <i class="gg-attribution"></i>
         <span>Trails</span>
     </div>
-    <div class="button button-large" name="PrimaryButton">
+    <div class="button button-large" data-ref="primaryButton">
         <img alt="a" src="https://img.icons8.com/ffffff/ios/50/mountain.png"/>
     </div>
-    <div class="button">
+    <div class="button" data-ref="pinsButton">
         <i class="gg-pin"></i>
         <span>Pins</span>
     </div>
-    <div class="button">
+    <div class="button" data-ref="moreButton">
         <i class="gg-more"></i>
         <span>More</span>
     </div>
