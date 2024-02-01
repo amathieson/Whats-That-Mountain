1b30a6bde6ec66aeda2a3467decc86f9753cce08
Adam Mathieson <adam@mathieson.dev>
2024-02-01 11:09:41 +0000

    + Sanity check the data

---
+++ _data/validate_tiles.php
@@ -0,0 +1,25 @@
+<?php
+$tiles = (array)json_decode(file_get_contents("processed_tiles.json"));
+
+echo "Loaded " . sizeof($tiles) . " tiles\n";
+
+foreach ($tiles as $id=>$tile) {
+    if (!isset($tile->peaks) || isset($tile->error)) {
+        echo "'$id' is missing data!\n";
+        continue;
+    }
+
+    $a = array_filter($tile->peaks);
+    $average = array_sum($a)/count($a);
+    if (abs($tile->peak - $average) > 150) {
+        echo "'$id' Peak∆ too high! - ";
+        print_r($tile);
+    }
+
+    $a = array_filter($tile->valleys);
+    $average = array_sum($a)/count($tile->valleys);
+    if (abs($tile->valley - $average) > 150) {
+        echo "'$id' Valley∆ too high! - ";
+        print_r($tile);
+    }
+}
