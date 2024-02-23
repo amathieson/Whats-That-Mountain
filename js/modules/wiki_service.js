b16e547a35394e6d29675b14bdaec633587917ce
Adam Mathieson <adam@mathieson.dev>
2024-02-23 18:03:22 +0000

    + UI Updates  + Index Pages on Wiki

--- js/modules/wiki_service.js
+++ js/modules/wiki_service.js
@@ -8,7 +8,7 @@ async function pull_data(poi) {
         let wikipage = null;
         let wikiimage = null;
         if (wikidata.sitelinks && wikidata.sitelinks.enwiki)
-            wikipage = await (await fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|pageimages&exintro&explaintext&redirects=1&pilicense=free&piprop=original|name&titles=${wikidata.sitelinks.enwiki.title}`)).json()
+            wikipage = await (await fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|pageimages&exintro&explaintext&redirects=1&pilicense=free&piprop=original|name&indexpageids&titles=${wikidata.sitelinks.enwiki.title}`)).json()
         if (wikipage && wikipage.query.pageids && wikipage.query.pages[wikipage.query.pageids[0]]) {
             wikiimage = await (await fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|imageinfo&iilimit=50&iiprop=timestamp|user|url|parsedcomment|commonmetadata&explaintext&redirects=1&titles=File:${wikipage.query.pages[wikipage.query.pageids[0]].pageimage}`)).json()
 
